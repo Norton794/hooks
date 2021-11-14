1d56ee4d7233cff740a69a1540647c137f7ac531
Norton <nortonsantos79@gmail.com>
2021-11-14 19:11:09 -0200

    useFetch

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 import { useCounter } from "../../hooks/useCounter";
-const UseRef = (props) => {
-    const [count, inc, dec] = useCounter()
+import { useFetch } from "../../hooks/useFetch";
+const UseCustom = (props) => {
+  const URL = 'http://files.cod3r.com.br/curso-react/estados.json'
+
+  const response = useFetch(URL);
+
+  function showStates(states) {
+    return states.map(state => <li key={state.nome}>{state.nome} - {state.sigla}</li>)
+  }
+
+  //fetch('http://files.cod3r.com.br/curso-react/estados.json').then(resp => resp.json()).then(json=>console.log(json))
+  const [count, inc, dec] = useCounter()
   return (
     <div className="UseCustom">
       <PageTitle
@@ -14,14 +24,22 @@ const UseRef = (props) => {
       <SectionTitle title="Exercicio 1" />
       <div className="center">
 
-<span className="text">{count}</span>
-<div>
-    <button className="btn" onClick={()=>inc()}>Inc</button>
-    <button className="btn" onClick={()=>dec()}>dec</button>
-</div>
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => inc()}>Inc</button>
+          <button className="btn" onClick={() => dec()}>dec</button>
+        </div>
+      </div>
+
+
+
+      <SectionTitle title="Exercicio 2" />
+      <div className="center">
+        <ul>{response.data ? showStates(response.data) : ""}</ul>
+
       </div>
     </div>
   );
 };
 
-export default UseRef;
+export default UseCustom;
